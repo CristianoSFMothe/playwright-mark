5add2a7baa7bc4e4b8979ae0855cb8d9d6bef03d
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2023-04-13 22:41:40 -0300

    :rocket: Manipulando a API

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,12 +1,15 @@
 import { test, expect } from "@playwright/test";
-import { faker} from "@faker-js/faker";
 
-test('deve pode cadastrar um nova tarefa',  async ({ page}) => {
+test('deve pode cadastrar um nova tarefa',  async ({ page, request}) => {
+  
+  const tasnkName = 'Ler um livro de TypeScript';
+  
+  await request.delete('http://localhost:3333/helper/tasks/' + tasnkName)
   
   await page.goto('http://localhost:3000');
   
   const inputTaaksName = page.locator('input[class*=InputNewTask]');
-  await inputTaaksName.fill(faker.lorem.word());
+  await inputTaaksName.fill(tasnkName);
   
   // await page.click('xpath=//button[contains(text(), "Create")]');
   await page.click('css=button >> text=Create');
